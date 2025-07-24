6c90b94b374458542e779f9b8b4146abf9a86ce0
albaraa-prog <albraa9021@gmail.com>
2025-07-24 14:59:31 +0400

    Updated solution for logic1-cigarParty

--- logic1/src/cigarParty.java
+++ logic1/src/cigarParty.java
@@ -1,15 +1,9 @@
 public class cigarParty {
     public boolean cigarParty(int cigars, boolean isWeekend) {
         if(isWeekend){
-            if(cigars >= 40 && cigars <= 60){
-                return true;
-            } else {
-                return false;
-            }
-        }else if(!isWeekend){
-            return true;
-        } else {
-            return false;
+            return cigars >= 40;
+        } else{
+            return cigars >= 40 && cigars <= 60;
         }
     }
 }
