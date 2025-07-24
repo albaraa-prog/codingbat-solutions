dda2e776949bad86db5ee2051d839c06d5b59f7b
albaraa-prog <albraa9021@gmail.com>
2025-07-24 15:13:39 +0400

    Updated solution for logic1-caughtSpeeding

--- logic1/src/cigarParty.java
+++
@@ -1,9 +0,0 @@
-public class cigarParty {
-    public boolean cigarParty(int cigars, boolean isWeekend) {
-        if(isWeekend){
-            return cigars >= 40;
-        } else{
-            return cigars >= 40 && cigars <= 60;
-        }
-    }
-}
