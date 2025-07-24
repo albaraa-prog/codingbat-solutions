947c99de5d99638f5393a46bde435b8f1491cf59
albaraa-prog <albraa9021@gmail.com>
2025-07-24 14:40:53 +0400

    Updated solution for logic1-dateFashion

--- logic1/src/dateFashion.java
+++ logic1/src/dateFashion.java
@@ -1,11 +1,11 @@
 public class dateFashion {
     public int dateFashion(int you, int date) {
-        if (you >= 8 || date >= 8){
-            return 2;
-        } else if(you >= 3 && you < 8 || date >= 3 && date < 8){
-            return 1;
-        } else {
+        if (you <= 2 || date <= 2){
             return 0;
+        } else if(you >= 8 || date >= 8){
+            return 2;
+        } else {
+            return 1;
         }
     }
 }
