9a7346dc05eeb7146ca8400afa3c47ad7111f63a
Al Baraa <albraa9021@gmail.com>
2025-07-26 20:46:06 +0400

    Add solution for logic1-sumLimit

---
+++ logic1/src/sumLimit.java
@@ -0,0 +1,8 @@
+public class sumLimit {
+    public int sumLimit(int a, int b) {
+        if(String.valueOf(a).length() == String.valueOf(a + b).length()){
+            return a + b;
+        }
+        return a;
+    }
+}
