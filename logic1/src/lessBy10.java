5809ef9d08efed043c81abf2379731315dbb7751
Al Baraa <albraa9021@gmail.com>
2025-07-26 18:20:26 +0400

    Add solution for logic1-lessBy10

---
+++ logic1/src/lessBy10.java
@@ -0,0 +1,10 @@
+public class lessBy10 {
+    public boolean lessBy10(int a, int b, int c) {
+        if ((Math.abs(a - b) >= 10)
+                || (Math.abs(a - c) >= 10)
+                || (Math.abs(b - c) >= 10)) {
+            return true;
+        }
+        return false;
+    }
+}
