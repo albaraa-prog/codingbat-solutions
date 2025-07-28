8a504f791150f18fe1c7740be77455c8d114167d
Al Baraa <albraa9021@gmail.com>
2025-07-29 01:55:46 +0400

    Add solution for string1-lastChars

---
+++ string-1/src/lastChars.java
@@ -0,0 +1,7 @@
+public class lastChars {
+    public String lastChars(String a, String b) {
+        char first = (a.length() > 0) ? a.charAt(0) : '@';
+        char last = (b.length() > 0) ? b.charAt(b.length() - 1) : '@';
+        return "" + first + last;
+    }
+}
