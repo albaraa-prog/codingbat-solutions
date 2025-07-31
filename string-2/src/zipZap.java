7cc2d6451958e25751f6fb103d4cbb969e11aed2
Al Baraa <albraa9021@gmail.com>
2025-08-01 01:18:21 +0400

    Add solution for string2-zipZap

---
+++ string-2/src/zipZap.java
@@ -0,0 +1,14 @@
+public class zipZap {
+    public String zipZap(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 2 < str.length() && str.charAt(i) == 'z' && str.charAt(i + 2) == 'p') {
+                result += "zp";
+                i += 2;
+            } else {
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
+}
