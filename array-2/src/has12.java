247f38488c951fd678fcb4c9599c028fc69fec61
Al Baraa <albraa9021@gmail.com>
2025-08-02 00:27:26 +0400

    Add solution for array2-has12

---
+++ array-2/src/has12.java
@@ -0,0 +1,14 @@
+public class has12 {
+    public boolean has12(int[] nums) {
+        boolean found1 = false;
+        for (int num : nums) {
+            if (num == 1) {
+                found1 = true;
+            }
+            if (found1 && num == 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
