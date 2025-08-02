4029a00c521bbdbb6e650344ba40d2ff9142c05e
Al Baraa <albraa9021@gmail.com>
2025-08-02 14:31:22 +0400

    Add solution for recursion1-array6

--- recursion-1/src/noX.java
+++ recursion-1/src/noX.java
@@ -1,11 +1,7 @@
 public class noX {
     public String noX(String str) {
-        if(str.length() == 0)
-            return str;
-
-        if(str.charAt(0) == 'x')
-            return noX(str.substring(1));
-
+        if(str.length() == 0) return str;
+        if(str.charAt(0) == 'x') return noX(str.substring(1));
         return str.charAt(0) + noX(str.substring(1));
     }
 }
