cd1a1c32d530ae6d3b149dda50c4698d0e50aafd
Al Baraa <albraa9021@gmail.com>
2025-08-02 14:00:06 +0400

    Add solution for recursion1-bunnyEars2

---
+++ recursion-1/src/bunnyEars2.java
@@ -0,0 +1,12 @@
+public class bunnyEars2 {
+    public static int bunnyEars2(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 3 + bunnyEars2(n - 1);
+        } else {
+            return 2 + bunnyEars2(n - 1);
+        }
+    }
+}
