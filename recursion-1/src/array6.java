4de1d364d67d22fa2c70ed9141fccc57295c94d2
Al Baraa <albraa9021@gmail.com>
2025-08-02 14:37:51 +0400

    Add solution for recursion1-array11

--- recursion-1/src/array6.java
+++ recursion-1/src/array6.java
@@ -2,7 +2,6 @@ public class array6 {
     public boolean array6(int[] nums, int index) {
         if(index >= nums.length)
             return false;
-
         return nums[index] == 6 || array6(nums, index + 1);
     }
 }
